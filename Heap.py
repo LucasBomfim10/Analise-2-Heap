5a926513e4c66f6999b72570172df72f8d8041e0
LUCAS BOMFIM <lucadez01@gmail.com>
2024-04-16 09:13:18 -0300

    v1

---
+++ Heap.py
@@ -0,0 +1,107 @@
+import math
+import time
+import random
+
+
+def makeheap(heap):
+    n = len(heap)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(heap, n, i)
+
+
+def heapify(heap, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and heap[left] > heap[largest]:
+        largest = left
+
+    if right < n and heap[right] > heap[largest]:
+        largest = right
+
+    if largest != i:
+        heap[i], heap[largest] = heap[largest], heap[i]
+        heapify(heap, n, largest)
+
+
+def insertheap(heap, value):
+    heap.append(value)
+    i = len(heap) - 1
+    while i > 0:
+        parent = (i - 1) // 2
+        if heap[parent] < heap[i]:
+            heap[parent], heap[i] = heap[i], heap[parent]
+            i = parent
+        else:
+            break
+
+
+def removeheap(heap):
+    if not heap:
+        return None
+    root = heap[0]
+    heap[0] = heap[-1]
+    del heap[-1]
+    heapify(heap, len(heap), 0)
+    return root
+
+
+def sqrt_sort_quadratico(vetor):
+    n = len(vetor)
+    k = math.floor(math.sqrt(n))
+    partes = []
+    vetor_ordenado = []
+
+    # Dividir o vetor em partes e criar um heap para cada parte
+    for i in range(0, n, k):
+        fim_parte = min(i + k, n)
+        if i + k >= n:
+            fim_parte = n
+        parte = vetor[i:fim_parte]
+        makeheap(parte)
+        partes.append(parte)
+
+    while any(partes):
+        # Encontrar o maior elemento de cada parte e comparar com o heap
+        maior_global = [-1]
+        makeheap(maior_global)
+        indice_global = None
+
+        for i, parte in enumerate(partes):
+            if parte:
+                if parte[0] > maior_global[0]:
+                    print(partes)
+                    print(parte[0])
+                    insertheap(maior_global, parte[0])
+                    indice_global = i
+
+        if indice_global is not None:
+            vetor_ordenado.insert(0, removeheap(maior_global))
+
+            removeheap(partes[indice_global])
+
+    return vetor_ordenado
+
+
+def main():
+
+    n = random.randint(10, 10)
+
+    vetor = [random.randint(1, 100) for _ in range(n)]
+
+    print("Vetor original:", vetor)
+    start_time = time.time()
+    tempos_execucao = []
+    for _ in range(3):
+        vetor_ordenado_quadratico = sqrt_sort_quadratico(vetor.copy())
+        tempo_execucao = time.time() - start_time
+        tempos_execucao.append(tempo_execucao)
+
+    media_tempo = sum(tempos_execucao) / len(tempos_execucao)
+    print("Média do tempo de execução:", media_tempo)
+    print("Vetor ordenado (método quadrático):", vetor_ordenado_quadratico)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Tradicional.py
@@ -0,0 +1,88 @@
+import math
+import time
+import random
+
+# Função de ordenação bubbleSort
+
+
+def bubbleSort(lista):
+    for num in range(len(lista)-1, 0, -1):
+        for i in range(num):
+            if lista[i] > lista[i+1]:
+                temp = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = temp
+
+
+def sqrt_sort_quadratico(vetor):
+    """
+    Args:
+        vetor (list): O vetor a ser ordenado.
+
+    Returns:
+        list: O vetor ordenado.
+    """
+
+    n = len(vetor)
+    k = math.floor(math.sqrt(n))
+    partes = []
+    vetor_ordenado = []
+    maior_global = -1
+    indice_global = [0, 0]
+
+    # Dividir o vetor em partes
+    for i in range(0, n, k):
+        fim_parte = min(i + k, n)
+        if i + k >= n:  # Check if it's the last part
+            fim_parte = n
+        partes.append(vetor[i:fim_parte])
+        bubbleSort(partes[-1])  # ordena as partes
+        # partes[-1].sort()
+
+    print(partes)
+    while any(partes):
+        # Encontrar o maior elemento de cada parte e comparar com o maior global
+        maior_global = float('-inf')  # Inicializar o maior global
+        for i, parte in enumerate(partes):
+            if parte:  # Verificar se a parte não está vazia
+                maior_parte = parte[-1]  # Pega o maior elemento da parte atual
+                if maior_parte > maior_global:
+                    maior_global = maior_parte
+                    # Atualiza o índice global
+                    indice_global = [i, len(parte) - 1]
+
+        if partes[indice_global[0]]:  # Verificar se a parte não está vazia
+            vetor_ordenado.insert(
+                0, partes[indice_global[0]].pop(indice_global[1]))
+        print(partes)
+        print(vetor_ordenado)
+
+    return vetor_ordenado
+
+
+def main():
+    """
+    Função principal para demonstração do algoritmo de ordenação por seleção de raiz quadrada.
+    """
+    n = random.randint(10, 10)
+
+    vetor = [random.randint(1, 100) for _ in range(n)]
+
+    print("Vetor original:", vetor)
+
+    start_time = time.time()
+    tempos_execucao = []
+    for _ in range(3):
+        vetor_ordenado_quadratico = sqrt_sort_quadratico(vetor.copy())
+        tempo_execucao = time.time() - start_time
+        tempos_execucao.append(tempo_execucao)
+
+    media_tempo = sum(tempos_execucao) / len(tempos_execucao)
+
+    print("Média do tempo de execução:", media_tempo)
+    print("Vetor ordenado (método quadrático):", vetor_ordenado_quadratico)
+
+
+if __name__ == "__main__":
+
+    main()
